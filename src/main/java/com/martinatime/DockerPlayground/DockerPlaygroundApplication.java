09c289ab85e992220191dbd0f58f98f1d92b9dfc
Aaron Martina <martinatime@gmail.com>
2023-10-15 21:43:47 -0400

    initial

---
+++ src/main/java/com/martinatime/DockerPlayground/DockerPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.martinatime.DockerPlayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerPlaygroundApplication.class, args);
+	}
+
+}
